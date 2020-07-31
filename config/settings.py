46a660257e296ba16e76b78c22f295b56efd313d
a-pompom <aoi.matsuda.3x@gmail.com>
2020-07-31 20:26:48 +0900

    Change HTML/Style
    公開する上で、スタイルが無い状態では見栄えがあまりよろしくなかったので、
    別アプリのスタイルを流用。

--- config/settings.py
+++ config/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
@@ -150,5 +151,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "templates/static/"),
+]
